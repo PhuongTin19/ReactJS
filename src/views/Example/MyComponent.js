1a2e316b29c570884749f40e4440a4bff74f2793
Nguyen Phuong Tin <tin63711@gmail.com>
2023-04-10 22:18:04 +0700

    components

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return <div> Hello my component </div>;
+    }
+}
+
+
+
+/*Dùng default để export 1 component, dùng { } export nhiều component*/
+export default MyComponent;
